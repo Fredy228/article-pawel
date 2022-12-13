debff742d47be693c849812193ab03392a013ac3
Fredy228 <smagrovich58@gmail.com>
2022-12-13 21:57:17 +0200

    scrollbar

--- src/js/murkap-artc-modal.js
+++ src/js/murkap-artc-modal.js
@@ -1,6 +1,7 @@
 import { refs } from './refs';
 
 export function markupArticlesModal(obj) {
+  window.scrollBy(0, 0);
   refs.modalArticle.innerHTML = '';
   const markup = [];
   markup.push(`<h2 class="modal__title">
@@ -19,19 +20,5 @@ export function markupArticlesModal(obj) {
     markup.push(`<p class="modal__text">${item}</p>`);
   });
   refs.modalArticle.id = `${obj.id}`;
-  //   markup.push(`<div id="disqus_thread"></div>
-  //       <script>
-  //           var disqus_config = function () {
-  //           this.page.url = "https://fredy228.github.io/article-pawel/";
-  //           this.page.identifier = '';
-  //           };
-  //           (function() {
-  //           var d = document, s = d.createElement('script');
-  //           s.src = 'https://articles-pawel.disqus.com/embed.js';
-  //           s.setAttribute('data-timestamp', +new Date());
-  //           (d.head || d.body).appendChild(s);
-  //           })();
-  //       </script>
-  //       <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>`);
   refs.modalArticle.insertAdjacentHTML('beforeend', markup.join(''));
 }
